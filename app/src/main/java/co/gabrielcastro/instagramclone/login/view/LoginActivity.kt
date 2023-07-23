45dd7e72543eea022afcbc1f32e29938aa054c16
Gabriel Castro <gabrielcastromail@gmail.com>
2023-07-23 15:15:57 -0300

    Initial commit

---
+++ app/src/main/java/co/gabrielcastro/instagramclone/login/view/LoginActivity.kt
@@ -0,0 +1,12 @@
+ package co.gabrielcastro.instagramclone.login.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import co.gabrielcastro.instagramclone.R
+
+ class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
